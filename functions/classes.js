dd3da5c6916b3ac1aea9374e7304e6ec40b29786
Matt McElwee <mmcelwee@dmsi.com>
2020-08-23 00:15:48 -0500

    Added support for next step class

--- functions/classes.js
+++ functions/classes.js
@@ -4,20 +4,41 @@ const catchify = require('catchify');
 const PCO_TOKEN = process.env.PCO_TOKEN;
 const PCO_SECRET = process.env.PCO_SECRET;
 
+const getTime = (str) => {
+  const dateString = str.replace('Next Step', '').trim();
+  const date = new Date(dateString);
+  const formatter = new Intl.DateTimeFormat('en-US', {
+    month: 'long',
+    year: 'numeric',
+    day: 'numeric'
+  });
+  return formatter.format(date);
+};
+
 exports.handler = async function(event, context, callback) {
-  const [err, data] = await catchify(got(`https://api.planningcenteronline.com/registrations/v2/events?filter=unarchived`, {
+  const { name } = event.queryStringParameters;
+  const opts = {
     username: PCO_TOKEN,
     password: PCO_SECRET,
-  }).json());
+  };
+  const isNextStep = name.includes('Next Step');
+  const url = isNextStep ?
+    `https://api.planningcenteronline.com/groups/v2/group_types/63283/groups` :
+    `https://api.planningcenteronline.com/registrations/v2/events?filter=unarchived`;
+
+  const [err, data] = await catchify(got(url, opts).json());
 
   if (err) return callback(err);
 
   return callback(null, {
     body: JSON.stringify(data.data
-      .filter(({ attributes }) => attributes.name.includes(event.queryStringParameters.name))
+      .filter(({ attributes }) => attributes.name.includes(isNextStep ? 'Next Step' : name))
       .map(({ attributes }) => ({
-        eventTime: attributes.event_time_summary,
-        publicUrl: attributes.public_url,
+        eventTime: isNextStep ? getTime(attributes.name) : attributes.event_time_summary,
+        publicUrl: isNextStep ?
+          `https://flatland.churchcenter.com/groups/core-classes/${attributes.name.replace(/\w/g, '-').toLowerCase()}` :
+          attributes.public_url,
+        schedule: isNextStep ? attributes.schedule : undefined,
       }))),
     statusCode: 200,
   });
