45438c5cf7099bf4399758cf8885bee828bfb849
Matt McElwee <mmcelwee@dmsi.com>
2020-08-22 22:54:26 -0500

    Updated function for debugging

--- functions/classes.js
+++ functions/classes.js
@@ -5,6 +5,7 @@ const PCO_TOKEN = process.env.PCO_TOKEN;
 const PCO_SECRET = process.env.PCO_SECRET;
 
 exports.handler = async function(event, context, callback) {
+  console.log({ PCO_TOKEN, PCO_SECRET })
   const [err, data] = await catchify(got(`https://api.planningcenteronline.com/registrations/v2/events?filter=unarchived`, {
     username: PCO_TOKEN,
     password: PCO_SECRET,
